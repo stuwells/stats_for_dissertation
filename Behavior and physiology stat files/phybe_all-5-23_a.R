52ee232d5297b2233eee177befb47831f843716e
stuwells <61471975+stuwells@users.noreply.github.com>
2021-08-21 21:34:59 -0700

    change estug to /10 instead of 1000 line 22 changed code to correct error

--- Behavior and physiology stat files/phybe_all-5-23_a.R
+++ Behavior and physiology stat files/phybe_all-5-23_a.R
@@ -9,7 +9,7 @@ estagr$fe_est<-as.numeric(as.character(estagr$fe_est))
 estagr$mal_agr<-as.numeric(as.character(estagr$mal_agr))
 estagr$bointun<-as.numeric(as.character(estagr$bointun))
 estagr$date=as.Date(estagr$date, format= "%d/%m/%y")
-estagr$fe_estug<-estagr$fe_est/1000
+estagr$fe_estug<-estagr$fe_est/10 
 estagr<-estagr[order(estagr$fe_est),]
 str(estagr)
 #agr1<-with(phybe[phybe$bointun!=0,],glm(b_agr~m_estug+I(m_estug^2),family="poisson",na.action="na.exclude"))
@@ -19,11 +19,14 @@ summary(agr1)
 plot(agr1)
 
 warnings()
-ggplot(agr1, aes(x = fe_estug,
-                y = mal_agr)) + geom_ribbon(aes(ymin = 0,
-                ymax = 5, fill = id ),
-                alpha = 0.2) + geom_line(aes(colour = id),
-                size = 1)                                                              
+ggplot(data = estagr, mapping = aes(x = fe_estug, y = mal_agr)) +
+  geom_ribbon(aes(ymin = 0, ymax = 5, fill = id ), alpha = 0.2) +
+  geom_line(mapping = aes(colour = id), size = 1)
+
+#ggplot(agr1, aes(x = fe_estug,
+#                y = mal_agr)) + geom_ribbon(aes(ymin = 0,
+#                alpha = 0.2) + geom_line(aes(colour = id),
+#                size = 1)                                                              
                 
 predictagr<-predict(agr1,se.fit=T)
 #ggtitle("Aggression Estradiol Relationship")
@@ -43,11 +46,11 @@ with(estagr[estagr$bointun!=0,],plot(mal_agr,fe_est),ylab="mal_agr",xlab="fe_est
 
 
 #ggplot(estagr,aes(estagr$bointun),ylim=4 (x=fe_est, color= mal_agr+geom_density(),method=glm()))
-ggplot(estagr, aes(x = fe_est, y = mal_agr)) +
+ggplot(estagr, aes(x = fe_estug, y = mal_agr) +
          geom_smooth()methods = glm(formula = mal_agr ~ mal_est_estug + I(mal_estug^2), family = "poisson", na.action = "na.exclude") 
                                                                     
 
-#with(estagr[estagr$bointun!=0,],xlab="Estradiol ng/gram",ylab="Agression")
+#with(estagr[estagr$bointun!=0,],xlab="Estradiol ng/gram",ylab="Aggression")
 #plot(y~x, main=" ", xlab=" ", ylab=" ")
 #abline(lm, col-"blue", lwd=1)
 #legend(x,y, c(" ", " "), title=" ", fill= "color", col="black")
@@ -59,16 +62,16 @@ ggplot(estagr, aes(x = fe_est, y = mal_agr)) +
 #est1<-est1+ylim(500,3500)
 #est1<-est1+ ggtitle("Individual Estradiol Levels")
 
- 
+##below contains code where mal_agr and fe_estug are reversed
 
-agr1<-with(estagr[estagr$bointun!=0,],glm(mal_agr~fe_estug+I(fe_estug^2),family="poisson",na.action="na.exclude"))
+agr1 <- with(estagr[estagr$bointun!= 0,],glm(mal_agr~fe_estug+I(fe_estug^2),family="poisson",na.action ="na.exclude"))
 summary(agr1)
-#dev.off()
-#jpeg("fig1.jpg",width=12.5, height=7.5, units="in",res=600)
-line.plot<- ggplot(data=estagr, aes(y="Aggression",x="Estradiol ng/gram"))
+dev.off()
+jpeg("fig1.jpg",width = 12.5, height = 7.5, units="in",res = 600)
+line.plot<- ggplot(data=estagr, aes(y="Aggression",x ="Estradiol ng/gram")
 line.plot
 line.plot+
- geom_line()
+geom_line()
 dev.off()
 
 
